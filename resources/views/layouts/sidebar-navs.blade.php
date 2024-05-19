d7b5c8a403e7bbb8aac191bd49a22fa3b9b66b79
Dreykovic <amonaaudrey16@gmail.com>
2024-05-19 09:56:39 +0000

    style:  add drop down

--- resources/views/layouts/sidebar-navs.blade.php
+++ resources/views/layouts/sidebar-navs.blade.php
@@ -7,14 +7,25 @@
                     <a href="{{ route('home') }}"><i data-feather="home"></i> <span>Dashboard</span></a>
                 </li>
 
+                <li class="submenu">
+                    <a class="{{ Str::startsWith(request()->path(), 'cotisations') ? 'active' : '' }}" href="#"><i
+                            data-feather="credit-card"></i><span> Cotisations</span>
+                        <span class="menu-arrow"></span></a>
+                    <ul>
+                        <li class="{{ Str::startsWith(request()->path(), 'cotisations/liste') ? 'active' : '' }}">
+                            <a href="{{ route('cotisations.index') }}">
+                                Liste</a>
+                        </li>
+                        <li class="{{ Str::startsWith(request()->path(), 'cotisations/add') ? 'active' : '' }}">
+                            <a href="{{ route('cotisations.create') }}">
+                                Ajouter</a>
+                        </li>
 
+                    </ul>
+                </li>
                 <li class="{{ Str::startsWith(request()->path(), 'natures') ? 'active' : '' }}">
                     <a href="{{ route('natures.index') }}"><i data-feather="star"></i> <span>Types
                             Cotisations</span></a>
-                </li>
-                <li class="{{ Str::startsWith(request()->path(), 'cotisations') ? 'active' : '' }}">
-                    <a href="{{ route('cotisations.index') }}"><i data-feather="credit-card"></i>
-                        <span>Cotisations</span></a>
                 </li>
 
 
